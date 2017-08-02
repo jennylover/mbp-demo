16e9d7b4089033eb665a97b58d8be17bc5b1f8fa
root <root@ip-172-31-5-231.ap-northeast-2.compute.internal>
2017-08-02 08:34:49 +0000

    mbp test1

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+require('vendor/autoload.php');
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+
+$s3 = S3Client::factory(['version' => '2006-03-01', 'region' => 'ap-northeast-1']);
+$bucket_in = "mbp-trans-input";
+$bucket_out = "mbp-trans-output";
+?>
+<html>
+    <head><meta charset="UTF-8"></head>
+    <body>
+        <h1>S3 upload example</h1>
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+    try {
+        $result = $s3->putObject(array(
+            'Bucket'       => $bucket_in,
+            'Key'          => $_FILES['userfile']['name'],
+            'SourceFile'   => $_FILES['userfile']['tmp_name'],
+            //'Body'         => fopen($_FILES['userfile']['tmp_name'], 'r+'),
+            //'ContentType'  => 'text/plain',
+            //'ACL'          => 'public-read',
+            // 'StorageClass' => 'REDUCED_REDUNDANCY',
+            'Metadata'     => array(
+                'param1' => 'value 1',
+                'param2' => 'value 2'
+            )
+        ));
+        //var_dump($result);
+?>
+        <p>Upload <a href="<?=htmlspecialchars($result['ObjectURL'])?>">successful</a> :)</p>
+<?php
+    } catch(Exception $e) {
+?>
+        <p>Upload error :(</p>
+<?php
+    }
+}
+?>
+        <h2>Upload a file</h2>
+        <form enctype="multipart/form-data" action="<?=$_SERVER['PHP_SELF']?>" method="POST">
+            <input name="userfile" type="file"><input type="submit" value="Upload">
+        </form>
+
+        <table width="70%" align="center" border="1">
+<?php
+try {
+    $result = $s3->listObjects(array('Bucket' => $bucket_out, 'Delimiter' => '/'));
+
+    foreach ($result['CommonPrefixes'] as $object) {
+        echo("<tr>");
+        echo("<td colspan='4'><img src='http://www.theisozone.com/images/icons/forum.png'> ".$object['Prefix']."</td>");
+        echo("</tr>");
+    }
+
+    foreach ($result['Contents'] as $object) {
+        echo("<tr>");
+        echo("<td width='50%'><img src='https://www.yourgenome.org/modules/file/icons/video-x-generic.png'> ".$object['Key']."</td>");
+        echo("<td width='30%'>".$object['LastModified'].['date']."</td>");
+        echo("<td width='15%'>".$object['Size']." Bytes</td>");
+        echo("<td width='5%' align='center'><a href='http://d231sc70bupmhp.cloudfront.net/".$object['Key']."' target='_new'><img src='http://www.zemra.de/images/03-media-logo/play-zemra.png'></td>");
+        echo("</tr>");
+    }
+} catch (S3Exception $e) {
+    echo $e->getMessage() . "\n";
+}
+?>
+        <table>
+    </body>
+</html>
